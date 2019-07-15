b72fa70baf115479d8991409abfc7f603fc1e85f
lianglihao <807478609@qq.com>
2019-07-15 21:58:47 +0800

    .

--- webpack-demo/src/main.js
+++ webpack-demo/src/main.js
@@ -6,6 +6,7 @@ window.jsonpTest = function() {
     const script = document.createElement('script')
     script.src='http://localhost:5000/api/jsonpTest?fun=getusers'
     document.getElementsByTagName('head')[0].appendChild(script)
+    this.console.log(123)
 }
 
 window.getusers = function(res) {
